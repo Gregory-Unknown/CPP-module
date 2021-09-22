a0985df95f90bbd349d2860db3f213eb1ff220ca
Gunshoot <Grrisha2593@gmail.com>
2021-09-22 11:26:55 +0300

    another day

--- day02/ex03/bsp.cpp
+++ day02/ex03/bsp.cpp
@@ -1,6 +1,11 @@
 #include "Point.hpp"
 
+
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
-
+	float abp = (b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX());
+	float bcp = (c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX());
+	float cap = (a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX());
+	if ((abp > 0 && bcp > 0 && cap > 0) || (abp < 0 && bcp < 0 && cap < 0)) return (true);
+	return (false);
 }
