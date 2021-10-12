4da7094abcaa8be6f066bcc613b1b50b9d7b06e3
Gunshoot <Grrisha2593@gmail.com>
2021-10-12 19:56:08 +0300

    Another one

--- day08/ex02/mutantstack.hpp
+++ day08/ex02/mutantstack.hpp
@@ -2,55 +2,55 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
-#include <string>
-#include <cstdint>
 #include <stack>
-// #include <cstdint>
-
-// template <typename T>
-// class MutantStack : public std::stack<T> {
-// public:
-// 	MutantStack();
-// 	MutantStack(const MutantStack<T> &st);
-// 	virtual ~MutantStack();
-// 	MutantStack<T> &operator=(const MutantStack<T> &st);
-// 	// typedef typename std::stack<T>::container_type::iterator iterator;
-// 	// typedef typename std::stack<T>::container_type::const_iterator const_iterator;
-// 	// typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-// 	// typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
-// 	std::stack<T>::container_type::iterator begin();
-// 	std::stack<T>::container_type::const_iterator begin() const;
-// 	std::stack<T>::container_type::reverse_iterator rbegin();
-// 	std::stack<T>::container_type::const_reverse_iterator rbegin() const;
-// 	std::stack<T>::container_type::iterator end();
-// 	std::stack<T>::container_type::const_iterator end() const;
-// 	std::stack<T>::container_type::reverse_iterator rend();
-// 	std::stack<T>::container_type::const_reverse_iterator rend() const;
-// };
 
 template <typename T>
-class MutantStack : public std::stack<T>
-{
+class MutantStack : public std::stack<T> {
 	public:
-		MutantStack();
-		MutantStack(MutantStack<T> const &other);
-		virtual ~MutantStack();
-		MutantStack<T> &operator=(MutantStack<T> const &other);
-
-		// Typedef
-		typedef typename std::stack<T>::container_type::iterator                   iterator;
-		typedef typename std::stack<T>::container_type::const_iterator             const_iterator;
-		typedef typename std::stack<T>::container_type::reverse_iterator           reverse_iterator;
-		typedef typename std::stack<T>::container_type::const_reverse_iterator     const_reverse_iterator;
-		// -------
-		iterator begin();
-		const_iterator begin();
-		reverse_iterator rbegin();
-		const_reverse_iterator  rbegin();
-		iterator end();
-		const_iterator end();
-		reverse_iterator rend();
-		const_reverse_iterator  rend() const;
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+	virtual ~MutantStack() {}
+	MutantStack &operator=(const MutantStack &other)
+	{
+		std::stack<T>::operator=(other);
+		return (*this);
+	}
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+	iterator begin()
+	{
+		return (this->c.begin());
+	}
+	const_iterator begin() const
+	{
+		return (this->c.begin());
+	}
+	reverse_iterator rbegin()
+	{
+		return (this->c.rbegin());
+	}
+	const_reverse_iterator rbegin() const
+	{
+		return (this->c.rbegin());
+	}
+	iterator end()
+	{
+		return (this->c.end());
+	}
+	const_iterator end() const
+	{
+		return (this->c.end());
+	}
+	reverse_iterator rend()
+	{
+		return (this->c.rend());
+	}
+	const_reverse_iterator rend() const
+	{
+		return (this->c.rend());
+	}
 };
 
-#endif;
+#endif
